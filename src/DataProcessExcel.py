8119867ad4097cea27118ef8ff2a83a6e1564110
sailzeng <fullsail@163.com>
2020-11-27 22:01:04 +0800

    合入最新的代码。venv

--- src/DataProcessExcel.py
+++ src/DataProcessExcel.py
@@ -1,8 +1,10 @@
 import win32com.client as win32
 
+
 class DataProcessExcel(object):
     def __init__(self):
         self.excel_app_ = None
+        self.work_book_ = None
         self.sheets_ = None
         self.sheet_ = None
 
@@ -14,12 +16,21 @@ class DataProcessExcel(object):
         return
 
     def quit(self):
-        '''
+        """
         退出EXCEL,
         :return:
-        '''
+        """
         self.excel_app_.Quit()
         return
+
+    def open(self, file_name):
+        """
+
+        :return:
+        """
+        self.work_book_ = self.excel_app_.Workbooks.Open(file_name)
+        return
+
 
 if __name__ == '__main__':
     print("Hello world!")
