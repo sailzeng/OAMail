65ca40fcfa537599be3b16b569c929e2bdce0d45
sailzeng <fullsail@163.com>
2020-12-03 21:35:25 +0800

    改名称。

--- src/batch_process_send.py
+++ src/batch_process_send.py
@@ -3,26 +3,34 @@
 
 
 class BatchProcessSend(object):
-    #
+    """
+    批处理进行发送处理
+    """
     class SearchReplaceColumn(object):
+        """
+        搜索替换的列号
+        """
         def __init__(self):
-            self._search_column = 0
-            self._replace_column = 0
+            self._column_search = 0
+            self._column_replace = 0
 
-    class BodyReplace(object):
+    class BodyReplaceStr(object):
+        """
+        搜索替换的语句
+        """
         def __init__(self):
             self._search = ""
             self._replace = ""
 
     def __init__(self):
-        #
-        self._start_read_row = 0
-        self._sender_column = 0
-        self._cc_column = 0
-        self._body_column = 0
-        self._attachment_column_list = []
-        self._search_replace_column_list = []
-
+        # EXCEL表的行列信息
+        self.row_caption = 0
+        self._row_start_read = 0
+        self._column_sender = 0
+        self._column_cc = 0
+        self._column_body = 0
+        self._column_attachment_list = []
+        self._column_search_replace_list = []
         # 发送单人
         self._sender = ""
         self._cc = ""
@@ -39,7 +47,8 @@ class BatchProcessSend(object):
         # Body 文本替换列表
         self._body_replace_list = []
 
-
+        self._outlook = OutlookMail()
+        self._excel = ExcelDataBase()
 
 if __name__ == '__main__':
     pass
